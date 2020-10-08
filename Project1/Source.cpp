089dd20bd14e17578518256d4165e8c930d3b284
Dmytro <d.andriychak@gmail.com>
2020-10-08 15:07:28 +0300

    fix

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -7,15 +7,15 @@ int main()
 {
 	double Pi = 4 * atan(1.);
 	double x;
-	double z1;
-	//double z2;
+	//double z1;
+	double z2;
 	cout << "x = ";
 	cin >> x;
 	
-	z1 = 2 * pow(sin(3 * Pi - 2*x), 2) * pow(cos(5 * Pi + 2*x), 2);
-	//z2 = (1 / 4) - (1 / 4) * sin((5 / 2) * Pi - 8*x);
+	//z1 = 2 * pow(sin(3 * Pi - 2*x), 2) * pow(cos(5 * Pi + 2*x), 2);
+	z2 = (1 / 4) - (1 / 4) * sin((5 / 2) * Pi - 8*x);
 	cout << endl;
-	cout << "z1 = " << z1 << endl;
-	//cout << "z2 = " << z2 << endl;	cin.get();
+	//cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;	cin.get();
 	return 0;
 }
